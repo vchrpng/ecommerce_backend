39f91b56dc1c18399fc8f8a8d92067c45c36448c
vchrpng <vchrpng@gmail.com>
2020-05-23 13:33:50 +0700

    feat(Order): use post charge from stripe

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,6 +1,7 @@
 import express from "express"
 import Order from "../models/order"
 import parseErrors from "../utils/parseErrors"
+import postCharge from './stripe'
 
 const router = express.Router()
 

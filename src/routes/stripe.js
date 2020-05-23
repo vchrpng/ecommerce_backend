44872c523fd99eeecb6f894026992ca5a0c79fd3
vchrpng <vchrpng@gmail.com>
2020-05-23 14:44:04 +0700

    feat(Stripe): add a route for stripe

--- src/routes/stripe.js
+++ src/routes/stripe.js
@@ -1,4 +1,7 @@
+import express from 'express'
 import Stripe from 'stripe'
+
+const router = express.Router()
 
 const stripe = new Stripe(process.env.STRIPE_SECRET)
 
@@ -31,5 +34,5 @@ async function postCharge(req, res) {
 router.post('/stripe/charge', postCharge)
 
   
-  module.exports = postCharge
+export default stripe 
   
